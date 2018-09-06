b0a4310915cd9fe98dbfdea0d87aadd655f8820a
Liang Xu (Wicresoft) <v-lixu@microsoft.com>
2018-09-06 10:27:02 +0800

    fix bug map not defined

--- src/RollingBallClient/assets/Scripts/BlockManager.ts
+++ src/RollingBallClient/assets/Scripts/BlockManager.ts
@@ -10,7 +10,7 @@ export default class BlockManager {
     private static blockList: Array<Block>;
 
     public static genLinkBlockList(lvl:number): Block[] {
-        if(this.maps.length == 0){
+        if(this.maps == null || this.maps.length == 0){
             this.fillMaps();
         }
 
